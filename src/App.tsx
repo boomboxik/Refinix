1b56c03eabf0fdaed6f6cb461b036f91824c2c69
boomboxik <9067506@gmail.com>
2024-01-20 20:46:20 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import dataProvider, {
+  GraphQLClient,
+  liveProvider,
+} from "@refinedev/nestjs-query";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                // dataProvider={dataProvider(gqlClient)}
+                // liveProvider={liveProvider(wsClient)}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                // authProvider={}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "DwRH3T-KMquWm-Mf7iiF",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;

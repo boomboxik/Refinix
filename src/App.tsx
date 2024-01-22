e18b52622f6e250ac1bf0a8dbbcefd62ea550783
boomboxik <9067506@gmail.com>
2024-01-22 22:04:22 +0300

    create new routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import { dataProvider, liveProvider } from "./providers";
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register } from "./pages"
 
 import routerBindings, {
   DocumentTitleHandler,
@@ -26,7 +27,7 @@ function App() {
                 liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                // authProvider={}
+                authProvider={authProvider}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -37,6 +38,10 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />}/>
+                  <Route path="/register" element={<Register />}/>
+                  <Route path="/login" element={<Login />}/>
+                  <Route path="/forgot-password" element={<ForgotPassword />}/>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />

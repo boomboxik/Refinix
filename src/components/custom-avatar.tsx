ca429095de73e5af0b5f7042291482de2fa5fc2f
boomboxik <9067506@gmail.com>
2024-01-28 23:54:52 +0300

    update custom-avatar.tsx & current-user.tsx

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,20 +1,22 @@
 import { Avatar as AntdAvatar, AvatarProps } from 'antd'
 
 type Props = AvatarProps & {
-    name: string 
+    name?: string 
 }
 
 const CustomAvatar = ({ name, style, ...rest }: Props) => {
     return (
         <AntdAvatar
-            alt='boobboxik'
+            alt={name}
             size="small"
             style={{ 
                 backgroundColor: '#87d068', 
                 display: 'flex',
                 alignItems: 'center',
-                border: 'none'
+                border: 'none',
+                ...style
             }}
+            {...rest}
         >
             {name}    
         </AntdAvatar>

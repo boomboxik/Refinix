d6e68a5ebcc2ac9949194c75f31d9a433335f858
boomboxik <9067506@gmail.com>
2024-02-01 18:24:42 +0300

    update App.tsx & home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,33 @@
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Row
+                gutter={[32, 32]}
+                style={{
+                    marginTop: '32px'
+                }}
+            >
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={8}
+                    style={{
+                        heihgt: '460px'
+                    }}
+                >
+                    CalendarUpcomingEvents
+                </Col>
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={8}
+                    style={{
+                        heihgt: '460px'
+                    }}
+                >
+                    DashboardDealsChart
+                </Col>
+            </Row>
+        </div>
     )
 }

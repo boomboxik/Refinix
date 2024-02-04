80ef4223d32980c31b3d2f45771ed16980c0adac
boomboxik <9067506@gmail.com>
2024-02-04 21:13:22 +0300

    update upcoming events card

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { CalendarOutlined } from '@ant-design/icons'
+import { Card, List } from 'antd'
+import { Text } from '../text'
+import { useState } from 'react'
 
 const UpcomingEvents = () => {
+    const [isLoading setIsLoading] = useState(true)
+
     return (
-        <div>UpcomingEvents</div>
+        <Card 
+            style={{ height: '100%' }} 
+            headStyle={{padding: '8px 16px'}} 
+            bodyStyle={{
+            padding: '0 1rem'}}
+            title={
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '8px'
+                }}>
+                    <CalendarOutlined/>
+                    <Text size="sm" style={{ marginLeft: '0.7rem' }}>
+                        Upcoming Events
+                    </Text>
+                </div>
+            }
+        >
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={Array.from({ length: 5 }).map((_, index) => ({
+                        id: index
+                    }))}
+                >
+
+                </List>
+            ) : (
+                <List>
+
+                </List>
+            )}
+        </Card>
     )
 }
 

246203f7271e1fdfd7ff1902b88b4004df477bbd
boomboxik <9067506@gmail.com>
2024-02-22 16:15:01 +0300

    update company list page

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,7 +1,40 @@
-import { List } from "@refinedev/antd"
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries"
+import { CreateButton, List, useTable } from "@refinedev/antd"
+import { useGo } from "@refinedev/core"
+import { Table } from "antd"
 
 export const CompanyList = () => {
+    const go = useGo()
+    const { tableProps, filters } = useTable({
+        resource: 'companies',
+        meta: {
+            gqlQuery: COMPANIES_LIST_QUERY
+        }
+    })
+
     return (
-        <List></List>
+        <List
+            breadcrumb={false}
+            headerButtons={() => (
+                <CreateButton
+                    onClick={() => {
+                        go({
+                            to: {
+                                resource: 'companies',
+                                action: 'create'
+                            },
+                            options: {
+                                keepQuery: true
+                            },
+                            type: 'replace'
+                        })
+                    }}
+                />
+            )}
+        >
+            <Table>
+
+            </Table>
+        </List>
     )
 }

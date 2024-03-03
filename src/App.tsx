2f202bd5d6b98b4a7b024a237f1765aa421075be
boomboxik <9067506@gmail.com>
2024-03-03 15:21:21 +0300

    update edit page

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
-import Edit from "./pages/company/edit";
+import EditPage from "./pages/company/edit";
 
 function App() {
   return (
@@ -62,7 +62,7 @@ function App() {
                       <Route path="/companies">
                         <Route index element={<CompanyList />} />
                         <Route path="new" element={<Create />} />
-                        <Route path="edit/:id" element={<Edit />} />
+                        <Route path="edit/:id" element={<EditPage />} />
                       </Route>
                   </Route>
                 </Routes>
--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,7 +1,36 @@
-const Edit = () => {
+import { Col, Form, Row } from "antd"
+import { Edit, useForm } from "@refinedev/antd"
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations"
+import CustomAvatar from "@/components/custom-avatar"
+import { getNameInitials } from "@/utilities"
+
+const EditPage = () => {
+    const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+        redirect: false,
+        meta: {
+            gqlMutation: UPDATE_COMPANY_MUTATION
+        }
+    })
+
+    const { avatarUrl, name } = queryResult?.data?.data || {}
+
     return (
-        
+        <div>
+            <Row gutter={[32, 32]}>
+                <Col xs={24} xl={12}>
+                    <Edit
+                        isLoading={formLoading}
+                        saveButtonProps={saveButtonProps}
+                        breadcrumb={false}
+                    >
+                        <Form {...formProps} layout="vertical">
+                            <CustomAvatar shape="square" src={avatarUrl} name={getNameInitials(name || '')} style={{width: 96, height: 96, marginBottom: '24px'}} />
+                        </Form>
+                    </Edit>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
-export default Edit
+export default EditPage

d5362069c9cd9dbf8bdca4a71e9cfc92ce1aed4a
boomboxik <9067506@gmail.com>
2024-03-14 02:19:33 +0300

    update to do list

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,15 +1,33 @@
 import { KanbanBoardContainer, KanbanBoard } from "@/components/tasks/kanban/board"
 import KanbanColumn from "@/components/tasks/kanban/column"
 import KanbanItem from "@/components/tasks/kanban/item"
+import { TASKS_QUERY } from "@/graphql/queries"
+import { useList } from "@refinedev/core"
 
 const List = () => {
+    const { data: tasks, isLoading: isLoadingTasks } = useList({
+        resource: 'tasks',
+        sorters: [
+            {
+                field: 'dueDate',
+                order: 'asc'
+            }
+        ],
+        pagination: {
+            mode: 'off'
+        },
+        meta: {
+            gqlQuery: TASKS_QUERY
+        }
+    })
+
     return (
         <>
             <KanbanBoardContainer>
                 <KanbanBoard>
                     <KanbanColumn>
                         <KanbanItem>
-
+                            This is to do
                         </KanbanItem>
                     </KanbanColumn>
                     <KanbanColumn>

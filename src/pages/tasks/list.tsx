3eec0e763cd9d54bee8daca4ad1e72d168923cf0
boomboxik <9067506@gmail.com>
2024-03-15 06:48:18 +0300

    update list.tsx

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,10 +1,29 @@
 import { KanbanBoardContainer, KanbanBoard } from "@/components/tasks/kanban/board"
 import KanbanColumn from "@/components/tasks/kanban/column"
 import KanbanItem from "@/components/tasks/kanban/item"
-import { TASKS_QUERY } from "@/graphql/queries"
+import { TASKS_QUERY, TASK_STAGES_QUERY } from "@/graphql/queries"
 import { useList } from "@refinedev/core"
 
 const List = () => {
+    const { data: stages, isLoading: isLoadingStages } = useList ({
+        resource: 'taskStages',
+        filters: [
+            {
+                field: 'title',
+                operator: 'in',
+                value: ['TO DO', 'IN PROGRESS', 'IN REVIEW', 'DONE']
+            }
+        ],
+        sorters: [
+            {
+                field: 'createdAt',
+                order: 'asc'
+            }
+        ],
+        meta: {
+            gqlQuery: TASK_STAGES_QUERY
+        }
+    })
     const { data: tasks, isLoading: isLoadingTasks } = useList({
         resource: 'tasks',
         sorters: [
@@ -13,6 +32,9 @@ const List = () => {
                 order: 'asc'
             }
         ],
+        queryOptions: {
+            enabled: !!stages
+        },
         pagination: {
             mode: 'off'
         },
